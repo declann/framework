e48c6f98e4f68530b6ef4f135d55a2201d1e049a
Philippe Rivière <fil@rezo.net>
2024-04-17 10:10:50 +0200

    slightly more realistic test

--- test/input/build/fragments/index.md
+++ test/input/build/fragments/index.md
@@ -1,6 +1,7 @@
 ---
-hello: front-matter
+author: "Some One"
 title: Testing fragment functions
+date: 2024-04-17
 meta: ["very", "much"]
 ---
 
--- test/input/build/fragments/observablehq.config.js
+++ test/input/build/fragments/observablehq.config.js
@@ -1,5 +1,14 @@
 export default {
   head: (data) => JSON.stringify({hello: "head", data}),
-  header: (data) => JSON.stringify({hello: "header", data}),
+  header: (data) => `
+  ${data.author ? `<div class="byline">${data.author}</div>`: ""}
+  ${data.date instanceof Date ? `<div class="date">${data.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    timeZone: "utc"
+  })}</div>` : ""}
+  `,
   footer: (data) => JSON.stringify({hello: "footer", data}),
 };
